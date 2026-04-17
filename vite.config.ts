421e3177d5bd4a3e8976204828f9149b1ead43ee
Alex Bennett <alex.bennett@imminently.co>
2026-04-17 10:59:43 +1000

    Set up github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/contrast-checker/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
